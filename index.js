f92b531a69bb83a02b169abc0d65f119f7ca0781
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-07-16 18:52:12 +0430

    the most important part completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import inquirer from 'inquirer';
-
+import qr from "qr-image";
+import fs from 'fs'
 inquirer
   .prompt([
    {message: "Please Enter an URL", name: "URL"},
@@ -7,6 +8,8 @@ inquirer
   .then((answers) => {
     // Use user feedback for... whatever!!
     let url=answers.URL;
+    var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream('url.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
